4c05d531af1c01eeea5b3f7844acc85563e89b06
Leowisd <32425638+Leowisd@users.noreply.github.com>
2018-12-03 11:47:46 -0600

    20181203001

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import optimization
 
+for i in range(100):
 
+    # domain: the range of each value in the solution, the range is decided on how many choices of each pair of two places can be choosed 
+    domain=[(0,3)]*(len(optimization.people)*2)
 
-# domain: the range of each value in the solution, the range is decided on how many choices of each pair of two places can be choosed 
-domain=[(0,3)]*(len(optimization.people)*2)
+    # run the optimization
+    s=optimization.annealingoptimize(domain,optimization.schedulecost)
 
-# run the optimization
-s=optimization.annealingoptimize(domain,optimization.schedulecost)
-
-# print the cost of final solution
-print('The schedule cost is ',optimization.schedulecost(s))
-print()
-
-# print the final solution
-optimization.printschedule(s)
+    # print the cost of final solution
+    # print('The schedule cost is ',optimization.schedulecost(s))
+    print(optimization.schedulecost(s))
+    # print()
+    
+    # print the final solution
+    # optimization.printschedule(s)
